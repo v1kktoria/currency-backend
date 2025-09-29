f91e917d1f31014b2def8af8604038f60d5ca506
v1kktoria <v.yanulevich4@gmail.com>
2025-09-29 11:16:36 +0300

    add TypeORM migrations

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,9 +10,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [__dirname + "/../entities/*.{ts,js}"],
-  migrations: [],
+  migrations: [__dirname + "/../migrations/*.{ts,js}"],
   subscribers: [],
 });
